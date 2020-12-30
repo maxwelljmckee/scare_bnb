bd8878ca7e9b944406cb435dd2c945650a2128cd
maxwelljmckee <maxwelljmckee@gmail.com>
2020-12-30 11:00:29 -0700

    change API key and remove from version control

--- app/api/houses_routes.py
+++ app/api/houses_routes.py
@@ -5,9 +5,18 @@ from app.forms import house_create_form
 houses_routes = Blueprint('houses', __name__)
 
 
-@houses_routes.route('/', method='GET')
-def get_houses():
-    request.json.get('name', None)
-    new_house = {k: v for k, v in request.json.items() if k not in ('csrf', '')}
-    db.session.add(new_house)
-    db.session.commit()
+@houses_routes.route('/states', method='GET')
+def get_all_states():
+    State.query.all()
+    
+
+
+
+
+
+
+# POST request
+    # request.json.get('name', None)
+    # new_house = {k: v for k, v in request.json.items() if k not in ('csrf', '')}
+    # db.session.add(new_house)
+    # db.session.commit()

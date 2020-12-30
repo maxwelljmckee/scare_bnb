347e0c58d9848f6edf1312b8928c952dfcaa52b7
maxwelljmckee <maxwelljmckee@gmail.com>
2020-12-30 11:00:06 -0700

    change API key and remove from version control

--- react-app/src/components/EmbedMap.js
+++ react-app/src/components/EmbedMap.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from 'react';
+require('dotenv').config()
 
+const MAPS_API_KEY = process.env.MAPS_API_KEY
 
 const EmbedMap = ({ queryString }) => {
   const encode = () => {
+    console.log(MAPS_API_KEY);
+    console.log(process.env);
     const encoded = queryString.split().join('+')
     return encoded
   }
@@ -13,7 +17,7 @@ const EmbedMap = ({ queryString }) => {
         title='googleMaps'
         width="1000"
         height="850"
-        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBWPv72iWYiEDzcxwy63ooJ7JC9obR8-iM&q=${encode(queryString)}`}>
+        src={`https://www.google.com/maps/embed/v1/place?key=${MAPS_API_KEY}&q=${encode(queryString)}`}>
       </iframe>
     </Fragment>
   )

b34354c92753e8605cd77baa5b04032369c98b66
Sam <samstarkscenic@gmail.com>
2020-12-30 19:23:39 -0500

    fixes logo loading issue

--- react-app/src/components/houses/HouseProfilePage.js
+++ react-app/src/components/houses/HouseProfilePage.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 const HouseProfilePage = () => {
   const { id } = useParams()
 
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [house, setHouse] = useState(null)
+
+  useEffect(() => {
+
+    const getHouse = async () => {
+      const response = await fetch(`/api/houses/${id}`)
+      const body = await response.json()
+      console.log(body)
+      setHouse(body)
+      setIsLoaded(true)
+    }
+    getHouse()
+  }, [id])
+
   return (
-    <div>stuff</div>
+    <div style={{ paddingTop: "60px" }}>
+      {!isLoaded && (
+        <div>Loading...</div>
+      )}
+      {house && isLoaded && (
+        <div> </div>
+      )}
+    </div>
   )
 }
 

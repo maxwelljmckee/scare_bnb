3aa05b26383524b7b52fb438d5d5c61b80b0d994
Mathias <mathiasanderson88@gmail.com>
2020-12-31 10:31:57 -0800

    House Form validation working

--- app/api/houses_routes.py
+++ app/api/houses_routes.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import db, House, State
-from app.forms import house_create_form
+from app.forms import HouseCreateForm
 
 houses_routes = Blueprint('houses', __name__)
 
-
+def validator_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
 
 @houses_routes.route('/')
 def get_houses():
@@ -24,28 +32,33 @@ def get_states():
 
 @houses_routes.route('create', methods=['POST'])
 def create_house():
-    print(request.json)
-    print(request.json)
-    host_id = request.json.get("hostId", None)
-    name = request.json.get("name", None)
-    street_1 = request.json.get("street1", None)
-    street_2 = request.json.get("street2", None)
-    city = request.json.get("city", None)
-    state_id = request.json.get("state", None)
-    postal_code= request.json.get("postalCode", None)
-    house_pic_url= request.json.get("housePicUrl", None)
-    description = request.json.get("description", None)
-    max_guests = request.json.get("maxGuests", None)
-    num_bedrooms = request.json.get("numBedrooms", None)
-    num_beds = request.json.get("numBeds", None)
-    num_baths = request.json.get("numBaths", None)
-    price = request.json.get("price", None)
+    form = HouseCreateForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        house = House( host_id=host_id, name=name, street_1=street_1, street_2=street_2, city=city, state_id=state_id, postal_code=postal_code, house_pic_url=house_pic_url, description=description, max_guests=int(max_guests), num_bedrooms=int(num_bedrooms), num_beds=int(num_beds), num_baths=int(num_baths), price=int(price))
+        db.session.add(house)
+        db.session.commit()
+        return house.to_dict()
+    return {'error': validator_errors_to_error_messages(form.errors)}
+
+    # host_id = request.json.get("hostId", None)
+    # name = request.json.get("name", None)
+    # street_1 = request.json.get("street1", None)
+    # street_2 = request.json.get("street2", None)
+    # city = request.json.get("city", None)
+    # state_id = request.json.get("state", None)
+    # postal_code= request.json.get("postalCode", None)
+    # house_pic_url= request.json.get("housePicUrl", None)
+    # description = request.json.get("description", None)
+    # max_guests = request.json.get("maxGuests", None)
+    # num_bedrooms = request.json.get("numBedrooms", None)
+    # num_beds = request.json.get("numBeds", None)
+    # num_baths = request.json.get("numBaths", None)
+    # price = request.json.get("price", None)
 
     # state = State.query.get(state)
-    house = House( host_id=host_id, name=name, street_1=street_1, street_2=street_2, city=city, state_id=state_id, postal_code=postal_code, house_pic_url=house_pic_url, description=description, max_guests=int(max_guests), num_bedrooms=int(num_bedrooms), num_beds=int(num_beds), num_baths=int(num_baths), price=int(price))
-    db.session.add(house)
-    db.session.commit()
-    return house.to_dict()
+
+
 
 
 
--- app/forms/house_create_form.py
+++ app/forms/house_create_form.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models import House
 
 
 class HouseCreateForm(FlaskForm):
-    pass
+    # host_id = IntegerField("hostId", validators=[DataRequired()])
+    name = StringField("name", validators=[DataRequired()])
+    street_1 = IntegerField("street1", validators=[DataRequired()])
+    street_2 = IntegerField("street2", validators=[DataRequired()])
+    city = StringField("city", validators=[DataRequired()])
+    state_id = IntegerField("stateId", validators=[DataRequired()])
+    postal_code = IntegerField("postalCode", validators=[DataRequired()])
+    # house_pic_url = IntegerField("stateId", validators=[DataRequired()])
+    description = StringField("description", validators=[DataRequired()])
+    max_guests = IntegerField("maxGuest", validators=[DataRequired()])
+    num_bedrooms = IntegerField("numBedrooms", validators=[DataRequired()])
+    num_beds = IntegerField("numBedrooms", validators=[DataRequired()])
+    num_baths = IntegerField("numBedrooms", validators=[DataRequired()])
+    price = IntegerField("description", validators=[DataRequired()])

34b531db3fafd9028b180ae0971d9c9487543aae
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-03 18:39:04 -0700

    debug and restyle meetDevs component

--- react-app/src/components/SplashPage/NavBar/index.js
+++ react-app/src/components/SplashPage/NavBar/index.js
@@ -15,24 +15,10 @@ const NavBar = ({ authenticated, setAuthenticated }) => {
         <Link to="/"> <img className="navbar__logo" src={spookyBoi} /> </Link>
 
       </div>
-      <div className="navbar__center">
-        <DevDropDown />
-        {/* <div>Meet the Devs: </div> */}
-        {/* <div style={{ width: "300px", height: "40px", borderRadius: "20px", border: "1px solid black", textAlign: "center" }}> Meet the Devs!</div>
-        <Link className="nav-devs" to="">
-          <p>Sam Stark</p>
-        </Link>
-        <Link className="nav-devs" to="">
-          <p>Maxwell McKee</p>
-        </Link>
-        <Link className="nav-devs" to="">
-          <p>Peter Jesse</p>
-        </Link>
-        <Link className="nav-devs" to="">
-          <p>Mathias Anderson</p>
-  </Link> */}
-      </div>
+      {/* <div className="navbar__center">
+      </div> */}
       <div className="navbar__right">
+        <DevDropDown />
         {authenticated && (
           <Link className="navbar__link" to="/listings/create">
             Become a host
